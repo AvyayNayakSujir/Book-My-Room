724b7b1c25e5b6aa7585ef37cd10a5cad986c36c
K Anirudh Shetty <anirushetty09@gmail.com>
2024-04-18 10:24:22 +0530

    hotelimage

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,7 +3,7 @@ const Navbar = () => {
   return (
     <div className="navbar flex h-[50px] justify-center bg-[#003580]">
       <div className="nav-container flex w-full max-w-[1024px] items-center justify-between text-white">
-        <span className="logo font-[500] text-[25px]">BookMyRoom</span>
+        <span className="logo font-[500] text-[25px]">HotelEase</span>
         <div className="navItems text-[17px] mt-[6px]" >
           <AuthShowcase />
         </div>
